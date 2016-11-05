951dfaad5824b1748bc3a21ff07cbd239a5f2856
richardpitargue <jlpitargue@up.edu.ph>
2016-11-06 03:01:22 +0800

    Add basic game engine

---
+++ src/game/gsm/GameStateManager.java
@@ -0,0 +1,35 @@
+package game.gsm;
+
+import java.awt.Graphics2D;
+import java.util.Stack;
+
+public class GameStateManager {
+	
+	private Stack<State> states;
+	
+	public GameStateManager() {
+		states = new Stack<State>();
+	}
+	
+	public void update(double delta) {
+		states.peek().update(delta);
+	}
+	
+	public void draw(Graphics2D g) {
+		states.peek().draw(g);
+	}
+	
+	public void changeState(State nextState, boolean shouldPop) {
+		if(states.size() > 0) {
+			states.peek().onExit();
+		}
+		
+		if(states.size() > 0 && shouldPop) {
+			states.pop();
+		}
+		
+		states.push(nextState);
+		states.peek().onEnter();
+	}
+	
+}
