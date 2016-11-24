cc6b928be28dd289bc7b7ebc65c18497f3f0a2a2
Anton-Renzo Benedict Reyes <antonrenzobenedict@gmail.com>
2016-11-24 13:09:32 +0800

    Collision

--- src/game/server/Server.java
+++ src/game/server/Server.java
@@ -183,7 +183,7 @@ public class Server implements Runnable {
 	
 	
 	public static void main(String[] args) {
-		Server server = new Server(4);
+		Server server = new Server(3);
 		server.run();
 	}
 
